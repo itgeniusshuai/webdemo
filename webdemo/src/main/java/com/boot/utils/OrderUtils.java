fb7de3ea818cefc22d80c58afe58948c45b05452
zhaoshuai3 <zhaoshuai3@xin.com>
2017-08-30 16:38:00 +0800

    add service

--- webdemo/src/main/java/com/boot/utils/OrderUtils.java
+++ webdemo/src/main/java/com/boot/utils/OrderUtils.java
@@ -1,10 +1,4 @@
 package com.boot.utils;
 
 public class OrderUtils {
-    // 参照58架构师生成id法则
-    public static long generateOrderId(){
-        // 39毫秒，4bit transcation ,2bit 机房，7bit 机器， 7bit 毫秒序列号
-
-        return 0;
-    }
 }
