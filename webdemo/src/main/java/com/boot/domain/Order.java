72c95697a89af8719f45448f1f0d904d3259cd0a
zhaoshuai3 <zhaoshuai3@xin.com>
2017-08-30 15:25:57 +0800

    add service

--- webdemo/src/main/java/com/boot/domain/Order.java
+++ webdemo/src/main/java/com/boot/domain/Order.java
@@ -1,17 +1,24 @@
-package com.boot.config;
+package com.boot.domain;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @Setter
 public class Order implements Serializable{
     // 订单id
     private String id;
+    // 卖家ID
     private Integer sellerId;
     private Integer buyerId;
-    private String productId;
+    private String goodsId;
+    private String totalAmount;
+    private Integer status;
+    private Date createTime;
+
+
 
 }
---
+++ webdemo/src/main/java/com/boot/enums/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.boot.enums;
+
+public enum OrderStatus {
+}
